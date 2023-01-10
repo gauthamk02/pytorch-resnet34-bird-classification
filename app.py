bbb36a7cf0d4080a4a6b6be1606173f03670b23a
Gautham Krishnan <gauthamkrishnanpriya@gmail.com>
2023-01-10 15:36:10 +0530

    Minor Changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ title = "Birds Classification - ResNet34 PyTorch"
 examples = [os.path.join(examples_dir, i) for i in os.listdir('examples')]
 
 interface = gr.Interface(fn=predict, inputs=gr.Image(type= 'numpy', shape=(224, 224)).style(height= 256),
-            outputs= gr.Label(num_top_classes= 5),
+            outputs= gr.Label(num_top_classes= 5), cache_examples= False,
             examples= examples, title= title, css= '.gr-box {background-color: rgb(230 230 230);}')
 
 interface.launch()
